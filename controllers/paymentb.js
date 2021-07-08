128ff07aca68da99f993e4bb05a01873f0e85fe5
prasad-gobburkar <prasadgobburkar@outlook.com>
2021-07-08 21:23:13 +0530

    first commit

---
+++ controllers/paymentb.js
@@ -0,0 +1,41 @@
+var braintree = require("braintree");
+
+var gateway = braintree.connect({
+  environment: braintree.Environment.Sandbox,
+  merchantId: "sbq9n3jd22j25jnk",
+  publicKey: "cwwn5wymm7w3vys9",
+  privateKey: "41c6bbd70f509b333588c49d14575992"
+});
+
+exports.getToken = (req, res) => {
+  gateway.clientToken.generate({}, function(err, response) {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send(response);
+    }
+  });
+};
+
+exports.processPayment = (req, res) => {
+  let nonceFromTheClient = req.body.paymentMethodNonce;
+
+  let amountFromTheClient = req.body.amount;
+  gateway.transaction.sale(
+    {
+      amount: amountFromTheClient,
+      paymentMethodNonce: nonceFromTheClient,
+
+      options: {
+        submitForSettlement: true
+      }
+    },
+    function(err, result) {
+      if (err) {
+        res.status(500).json(error);
+      } else {
+        res.json(result);
+      }
+    }
+  );
+};
